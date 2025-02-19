305ed527f12d39147e877240006d5fbd13a8fb06
2Revol2 <maksimbaluta02@gmail.com>
2025-02-19 18:17:28 +0300

    fix: fix bugs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import { Header } from "@/widgets/Header";
 import { Sidebar } from "@/widgets/Sidebar";
 import { Layout } from "antd";
-import { AppRouter } from "./providers/RouterProvider/index";
 import { Content } from "antd/es/layout/layout";
 import { useState } from "react";
+import { AppRouter } from "./providers/RouterProvider";
 export const App = () => {
   const [isOpen, setIsOpen] = useState(true);
   return (

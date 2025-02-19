7fa25b1ecef02ad5fb7fe40539def27693de38fd
2Revol2 <maksimbaluta02@gmail.com>
2025-02-19 17:17:26 +0300

    feat: update antd theme config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,38 +1,18 @@
 import { Header } from "@/widgets/Header";
 import { Sidebar } from "@/widgets/Sidebar";
 import { Layout } from "antd";
-import { AppRouter } from "./providers/routerProvider";
+import { AppRouter } from "./providers/RouterProvider";
 import { Content } from "antd/es/layout/layout";
 import { useState } from "react";
 export const App = () => {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <div>
-      <Layout
-        style={{
-          minHeight: "100vh",
-          backgroundColor: "#000000",
-          overflowX: 'hidden'
-        }}
-      >
+      <Layout className="wrapper">
         <Header isOpen={isOpen} setIsOpen={setIsOpen} />
-        <Layout
-          style={{
-            backgroundColor: "#000000",
-            padding: "0 5px",
-          }}
-        >
+        <Layout className="contentWrapper">
           <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
-          <Content
-            style={{
-              margin: "0 10px",
-              backgroundColor: "#101010",
-              padding: "10px",
-              borderRadius: "5px",
-              overflow: "auto",
-              height: "calc(100vh - 64px)",
-            }}
-          >
+          <Content className="content">
             <AppRouter />
           </Content>
         </Layout>
--- src/app/providers/AntDesign/antDesign.ts
+++ src/app/providers/AntDesign/antDesign.ts
@@ -1,9 +1,25 @@
-const Pagination = {
-  itemActiveBg: "#e50914",
-  colorText: "#e0e0e0",
-  itemBg: "#1f1f1f",
-};
 const config = {
-  Pagination,
+  token: {
+    colorTextDisabled: "#c0c0c0",
+    colorPrimary: "#e50914",
+    colorTextPlaceholder: "#e0e0e0",
+    colorBorder: "#000000",
+    colorTextQuaternary: "#e0e0e0",
+    colorText: "#e0e0e0",
+    colorBgElevated: "#1f1f1f",
+  },
+  components: {
+    Pagination: {
+      itemActiveBg: "#1f1f1f",
+      colorText: "#e0e0e0",
+      itemBg: "#1f1f1f",
+    },
+    Select: {
+      selectorBg: " #1f1f1f",
+      hoverBorderColor: "#000000",
+      optionSelectedBg: "#e50914",
+      optionActiveBg: '#1c1c1c'
+    },
+  },
 };
 export { config };

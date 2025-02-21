5a2c0601bb7fa0089f805c646f737f2331b82fc1
2Revol2 <maksimbaluta02@gmail.com>
2025-02-21 17:28:09 +0300

    fix: fix video player

--- src/features/VideoPlayer/ui/VideoPlayer.tsx
+++ src/features/VideoPlayer/ui/VideoPlayer.tsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
 import s from "./VideoPlayer.module.scss";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 export const VideoPlayer = () => {
   const [scriptHMTL, setScriptHTML] = useState("");
   const dataUrl = window.location.href;
+  const { id } = useParams();
   useEffect(() => {
     axios
       .get(
-        "//js.espanplay.site/get_player?w=610&h=370&type=widget&kp_id=&players=apicollaps,videocdn,hdvb,bazon,alloha,ustore,kodik,trailer&r_id=videoplayers&ani=COLLAPS&ati=&adi=&vni=VIDEOCDN&vti=&vdi=&hni=HDVB&hti=&hdi=&bni=BAZON&bti=&bdi=&alni=ALLOHATV&alti=&aldi=&usni=USTOREBZ&usti=&usdi=&koni=KODIK&koti=&kodi=&tti=&ru=" +
+        `//js.espanplay.site/get_player?w=610&h=370&type=widget&kp_id=${id}&players=apicollaps,videocdn,hdvb,bazon,alloha,ustore,kodik,trailer&r_id=videoplayers&ani=COLLAPS&ati=&adi=&vni=VIDEOCDN&vti=&vdi=&hni=HDVB&hti=&hdi=&bni=BAZON&bti=&bdi=&alni=ALLOHATV&alti=&aldi=&usni=USTOREBZ&usti=&usdi=&koni=KODIK&koti=&kodi=&tti=&ru=` +
           dataUrl
       )
       .then((res) => {
-        setScriptHTML(res.data.match(/<iframe.*<\/iframe>/gm)[1])
+        setScriptHTML(res.data.match(/<iframe.*<\/iframe>/gm)[1]);
       });
   }, []);
 
